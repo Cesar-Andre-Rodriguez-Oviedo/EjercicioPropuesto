df268deb1030f2f385710b9ca70fedd30e805aa8
Cesar <cesardecesss@gmail.com>
2021-02-09 12:50:24 -0500

    Repositorio

---
+++ registrocliente.php
@@ -0,0 +1,17 @@
+<?php
+
+$nombre = $_POST["Nombre"];
+$apelido = $_POST["Apellido"];
+$dni = $_POST["DNI"];
+$direccion = $_POST["Direccion"];
+$telefono = $_POST["Telefono"];
+
+echo($nombre);
+echo($apelido);
+echo($dni);
+echo($direccion);
+echo($telefono);
+
+
+
+?>
